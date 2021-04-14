36054edd8f064509951248166c70ca0d0d16544b
=Pedro Iván Fernández González <=pifernandez@uci.cu>
2021-04-14 09:51:00 -0400

    add: authentication validation (still working on it)

--- mywriter/writer/views.py
+++ mywriter/writer/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth import authenticate, login
 from .models import Writing
 
 INTRODUCTION = 'Welcome to myWriter! A web app based on python framework Django \
@@ -11,13 +13,22 @@ def index(request):
     return render(request,'login.html')
 
 def showWritings(request):
+    username = request.POST.get('author')
+    password = request.POST.get('password')
+    
+    user = authenticate(request, username=username, password=password)
+    
+    if user:
+        login(request, user)
+        values_for_template = {}
+        writings = Writing.objects.all().order_by('creation_date').reverse()
 
-    values_for_template = {}
-    writings = Writing.objects.all().order_by('creation_date').reverse()
+        values_for_template['title_placeholder'] = TITLE_PLACEHOLDER
+        values_for_template['body_placeholder'] = BODY_PLACEHOLDER
+        values_for_template['writings'] = writings
+        values_for_template['introduction'] = INTRODUCTION
 
-    values_for_template['title_placeholder'] = TITLE_PLACEHOLDER
-    values_for_template['body_placeholder'] = BODY_PLACEHOLDER
-    values_for_template['writings'] = writings
-    values_for_template['introduction'] = INTRODUCTION
-
-    return render(request,'writings_list.html', values_for_template)
+        return render(request,'writings_list.html', values_for_template)
+    
+    else:
+        raise PermissionDenied
