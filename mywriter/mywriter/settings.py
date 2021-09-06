5dc83d34a188ed19fb0ebe74014a100e3b5a92a5
Pedro Ivan Fernandez Gonzalez <pedroifgonzalez@gmail.com>
2021-09-06 16:54:33 -0400

    Add rest_framework app

--- mywriter/mywriter/settings.py
+++ mywriter/mywriter/settings.py
@@ -38,7 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'writer.apps.WriterConfig',
+    'rest_framework'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -77,7 +83,7 @@ WSGI_APPLICATION = 'mywriter.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
